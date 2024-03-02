038553a7d66d694a167c3a4592a0f12d5d842e73
Jean~ <jeanpedrosilva24@gmail.com>
2024-03-01 22:36:41 -0300

    estrutura com html e formulário

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,5 +1,17 @@
 from app import app
+from flask import render_template, request
 
 @app.route("/")
 def main():
-    return "<h1>hello world</h1>"
+    return render_template('index.html')
+
+@app.route("/login")
+def italo_gay():
+    return render_template('login.html')
+
+@app.route("/submit", methods=['GET', 'POST'])
+def submit_data():
+  if request.method == 'POST':
+        # Obter os dados do formulário
+        name = request.form['form_name']
+        return render_template('teste.html', name = name)
