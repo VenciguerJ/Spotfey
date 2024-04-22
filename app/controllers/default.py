6b5687006c44cb2f35020a00e13b005490c11fcc
Jean~ <jeanpedrosilva24@gmail.com>
2024-04-21 21:45:34 -0300

    Iniciando projeto de login

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,25 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from app import app
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route('/success', methods=['GET', 'POST'])
+def successful_login():
+    from app.models.database_config import cadastra_user
+
+    if request.method == 'POST':
+        user = request.form['form_user']
+        email = request.form['form_email']
+        photo = request.form['form_profile_picture']
+        password = request.form['form_password']
+    
+    msg = cadastra_user(user, password, email)
+    msg = str(msg)
+
+    return render_template('login_success.html', message=msg)
--- app/models/database_config.py
+++ app/models/database_config.py
@@ -1,16 +1,40 @@
 import mysql.connector
 
-class Config:
+class config:
     DB_HOST = 'localhost'
     DB_USER = 'root'
     DB_PASSWORD = '1234'
-    DB_NAME = 'seu_banco_de_dados'
+    DB_NAME = 'spotfei'
+    DB_PORT = '3306'
 
 def connect_to_database():
     connection = mysql.connector.connect(
-        host=Config.DB_HOST,
-        user=Config.DB_USER,
-        password=Config.DB_PASSWORD,
-        database=Config.DB_NAME
+        host=config.DB_HOST,
+        port=config.DB_PORT,
+        user=config.DB_USER,
+        password=config.DB_PASSWORD,
+        database=config.DB_NAME
     )
     return connection
+
+def cadastra_user(username, senha, email, foto_perfil=None):
+    connection = connect_to_database()
+    cursor = connection.cursor()
+    try:
+        if foto_perfil:
+            cursor.execute(f"insert into users (username, senha, foto_perfil, email) values ('{username}', '{senha}', '{foto_perfil}', '{email}')")
+        else:
+            cursor.execute(f"insert into users (username, senha, email) values ('{username}', '{senha}', '{email}')")
+    
+        connection.commit()
+     
+        resultado = 'Usuário cadastrado!'
+      
+        
+    except Exception as ex:
+        resultado = f'Erro no cadastro do usuário: {ex}'
+    finally:
+        cursor.close()
+        connection.close()
+
+    return resultado
