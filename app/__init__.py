d3580f2873544d3c93a214bcae993ced29a32f87
Jean Vencigueri <107017702+VenciguerJ@users.noreply.github.com>
2025-02-19 21:10:04 -0300

    Delete app directory

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '123'
-
-# Importar configurações
-from app.controllers import default
-from app.controllers.config import Config
